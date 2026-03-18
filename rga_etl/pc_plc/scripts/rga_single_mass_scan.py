050a4bc6c26eb97dac6568511631c0c049067470
Dacheng Xu <dx2227@columbia.edu>
2026-03-18 17:35:11 -0400

    Single mass scan using PC-PLC connection

---
+++ rga_etl/pc_plc/scripts/rga_single_mass_scan.py
@@ -0,0 +1,36 @@
+import os
+import logging
+
+import paho.mqtt.client as mqtt
+
+
+# -------------------------
+# Config
+# -------------------------
+HTTP_PORT = 8080
+MQTT_BROKER = os.getenv("MQTT_BROKER", "169.254.11.119")
+MQTT_PORT = int(os.getenv("MQTT_PORT", "1883"))
+MQTT_TOPIC_PREFIX = "brx/control"
+
+# -------------------------
+# MQTT setup
+# -------------------------
+mqtt_client = mqtt.Client()
+mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60)
+mqtt_client.loop_start()
+logging.info(f"Connected to MQTT broker at {MQTT_BROKER}:{MQTT_PORT}")
+
+command = {
+    "main": "ID?\r",
+    "length": 128,
+    "noresult": False
+    # commented out because RGA can not change timeout
+    # "timeout": 10000,  # in milliseconds
+}
+
+for key, value in command.items():
+    topic = f"{MQTT_TOPIC_PREFIX}/{key}"
+    try:
+        mqtt_client.publish(topic, str(value))
+    except Exception as e:
+        logging.error(f"Failed to publish to {topic}: {e}")
